c17161a16bc91c83b5b7869fe54f344827f8fd40
YuHryshchenko <julia.grischenko.2011@gmail.com>
2025-06-15 13:32:42 -0400

    update server

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/zpi-zp41_appWEB3_HryshchenkoYuliia_KPI_2025/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        timer: '1-timer.html',
+        snackbar: '2-snackbar.html',
+        imageSearch: '3-image-search.html',
+      },
+    },
+  },
+});
