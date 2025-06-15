b8da3707e96123826c8928a63ede8642f065c9c9
YuHryshchenko <julia.grischenko.2011@gmail.com>
2025-06-15 14:37:27 -0400

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
+  plugins: [react()],
   base: '/zpi-zp41_appWEB3_HryshchenkoYuliia_KPI_2025/',
   build: {
     rollupOptions: {
